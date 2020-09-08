17dbbcdd8ddc61648521f3c9b583717ac3708fa9
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-08 17:32:17 +0900

    fix

--- offroad/js/components/Settings/Settings.js
+++ offroad/js/components/Settings/Settings.js
@@ -678,7 +678,7 @@ class Settings extends Component {
                                     title='HKG Long Control 활성화'
                                     value={ !!parseInt(longControlEnabled) }
                                     iconSource={ Icons.openpilot }
-                                    description='warrings: it is beta, be carful!! Openpilot will control the speed of your car'
+                                    description='경고 : 이 기능은 베타기능이며 오픈파일럿이 속도를 컨트롤하기때문에 주의가 필요합니다.'
                                     isExpanded={ expandedCell == 'longcontrol_enabled' }
                                     handleExpanded={ () => this.handleExpanded('longcontrol_enabled') }
                                     handleChanged={ this.props.setLongControlEnabled } />
@@ -689,7 +689,7 @@ class Settings extends Component {
                                     title='HKG MAD 모드 활성화'
                                     value={ !!parseInt(madModeEnabled) }
                                     iconSource={ Icons.openpilot }
-                                    description='Only for Car without long control, Openpilot will engage when turn cruise control on'
+                                    description='Long Control 미사용 차량에 한하여 사용가능하며 크루즈버튼으로 오픈파일럿이 활성화됩니다.'
                                     isExpanded={ expandedCell == 'madMode_enabled' }
                                     handleExpanded={ () => this.handleExpanded('madMode_enabled') }
                                     handleChanged={ this.props.setMadModeEnabled } />
@@ -700,7 +700,7 @@ class Settings extends Component {
                                     title='자동차선변경 활성화'
                                     value={ !!parseInt(autoLaneChangeEnabled) }
                                     iconSource={ Icons.openpilot }
-                                    description='warrings: it is beta, be carful!!'
+                                    description='경고 : 이 기능은 베타기능이며 안전을위해 측후방감지기능이 있는 차량만사용하세요.'
                                     isExpanded={ expandedCell == 'autoLaneChange_enabled' }
                                     handleExpanded={ () => this.handleExpanded('autoLaneChange_enabled') }
                                     handleChanged={ this.props.setAutoLaneChangeEnabled } />
