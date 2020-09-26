2edd9abca2baef509f818e5d99658f6e2f6ebb37
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-26 22:24:42 +0900

    Update UpdatePrompt.js

--- offroad/js/components/UpdatePrompt/UpdatePrompt.js
+++ offroad/js/components/UpdatePrompt/UpdatePrompt.js
@@ -25,8 +25,8 @@ class UpdatePrompt extends Component {
                 <ScrollThrough
                     onPrimaryButtonClick={ this.onUpdatePressed }
                     onSecondaryButtonClick={ this.props.dismiss }
-                    primaryButtonText={ '재부팅후 업데이트' }
-                    secondaryButtonText={ '나중에' }
+                    primaryButtonText={ '업데이트' }
+                    secondaryButtonText={ '취소' }
                     onScroll={ this.onScroll }
                     enabled={ true }>
                     <X.Text color='white' size='big' weight='semibold'>Update Available</X.Text>
