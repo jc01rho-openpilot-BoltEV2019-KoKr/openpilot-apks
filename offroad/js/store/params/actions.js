06cc4a12126d51c3b04edec13ed11fe0cec06ba0
Woohyun Rho <rkjnice@gmail.com>
2020-09-27 23:27:25 +0900

    Feature/add prebuiltbutton (#5)
    * Update Home.js
    
    * Update Settings.js
    
    * Update SetupQr.js
    
    * Update SetupTerms.js
    
    * Update UpdatePrompt.js
    
    * Update Onboarding.js
    
    * Add files via upload
    
    * Update Settings.js
    
    * Update SetupWifi.js
    
    * update
    
    * toggle kor
    
    * Update Settings.js
    
    * Create main.yml
    
    * fix & info
    
    * fix
    
    * Delete main.yml
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Home.js
    
    * Update Home.js
    
    * git pull add @jc01rho
    
    * git pull add @jc01rho
    
    * Add files via upload
    
    * Add files via upload
    
    * Update config.js
    
    * Update Settings.js
    
    * Update actions.js
    
    * Update actions.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * Update Settings.js
    
    * working copy
    
    * test for build
    
    * Create main.yml
    
    * test
    
    * test for build
    
    * Update Settings.js
    
    * Create main.yml
    
    * git pull button added.
    
    * add git pull button
    
    * rename build recipe
    
    * fix notify onProgress..
    
    * fix notify onProgress..
    
    * fix notify onProgress..
    
    * fix notify onProgress..
    
    * fix try not to call reactmethod
    
    * fix reboot double call
    
    * fix for process waits.
    
    * fix codes for kotlin style
    
    * test for buildcache
    
    * fix kotlin style...
    
    * update for cache
    
    * update for cache
    
    * update for cache
    
    * fix typo.
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * defaults
    
    * fix readme
    
    * fix sleep 10 to 5
    
    * testing..
    
    * testing..
    
    * Update Settings.js
    
    * Update main.yml
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    Co-authored-by: whrho <whrho@fasoo.com>
    
    * hotfixes
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * testing..
    
    * remove style test
    
    * add button actions
    
    * git pull&reboot button to delete prebuilt
    
    * fix conditional buttons
    
    * fix redunduant
    
    * toggle buttons
    
    * test toast
    
    * fix conidition
    
    * remove async attributes
    
    * fix rendersettings
    
    * revert comments
    
    * fix button typoes
    
    Co-authored-by: Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
    Co-authored-by: xx979xx <xx979xx@github.com>
    Co-authored-by: whrho <whrho@fasoo.com>

--- offroad/js/store/params/actions.js
+++ offroad/js/store/params/actions.js
@@ -48,7 +48,8 @@ const PARAMS = [
   "LongControlEnabled",
   "MadModeEnabled",
   "AutoLaneChangeEnabled",
-  "IsRHD"
+  "IsRHD",
+  "PutPrebuiltOn"
 ].concat(ALERT_PARAMS);
 
 export function refreshParams(params) {
